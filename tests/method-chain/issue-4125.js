4b2533fb1aef382161071b38bdaeef07290b01f6
mmkal <15040698+mmkal@users.noreply.github.com>
2019-10-19 15:01:16 -0400

    more examples from issue 1565

--- tests/method-chain/issue-4125.js
+++ tests/method-chain/issue-4125.js
@@ -35,3 +35,36 @@ const parseNumbers = s => s.split('').map(Number).sort()
 function palindrome(a, b) {
   return a.slice().reverse().join(',') === b.slice().sort().join(',');
 }
+
+// https://github.com/prettier/prettier/issues/1565
+
+d3.select("body").selectAll("p").data([1, 2, 3]).enter().style("color", "white");
+
+Object.keys(props).filter(key => key in own === false).reduce((a, key) => {
+  a[key] = props[key];
+  return a;
+}, {})
+
+point().x(4).y(3).z(6).plot();
+
+assert.equal(this.$().text().trim(), '1000');
+
+something().then(() => doSomethingElse()).then(result => dontForgetThisAsWell(result))
+
+db.branch(
+  db.table('users').filter({ email }).count(),
+  db.table('users').insert({ email }),
+  db.table('users').filter({ email }),
+)
+
+sandbox.stub(config, 'get').withArgs('env').returns('dev')
+
+moment.utc(userInput).hour(0).minute(0).second(0)
+
+fetchUser(id)
+  .then(fetchAccountForUser)
+  .catch(handleFetchError)
+
+fetchUser(id) //
+  .then(fetchAccountForUser)
+  .catch(handleFetchError)
