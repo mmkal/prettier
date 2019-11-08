2acaee658f05882e89127a717e0f83604d8ac2f1
mmkal <15040698+mmkal@users.noreply.github.com>
2019-11-07 23:03:04 -0500

    repro optional chaining bug

--- src/language-js/printer-estree.js
+++ src/language-js/printer-estree.js
@@ -5100,7 +5100,7 @@ function printMemberChain(path, options, print) {
   // If the last group is a function it's okay to inline if it fits.
   if (
     hasComment ||
-    callExpressions.length >= 3 ||
+    callExpressions.length >= 4 ||
     printedGroups.slice(0, -1).some(willBreak) ||
     /**
      *     scopes.filter(scope => scope.value !== '').map((scope, i) => {
